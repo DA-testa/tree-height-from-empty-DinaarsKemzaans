9d2f86c527aaba975d3bf3277437f3c9f40e62a0
Dinārs Kemzāns 221RDB321 <125220545+DinaarsKemzaans@users.noreply.github.com>
2023-03-01 15:56:02 +0000

    geting result?

--- tree_height.py
+++ tree_height.py
@@ -1,4 +1,4 @@
-# python3
+#Dinārs Kemzāns 221RDB321
 
 import sys
 import threading
@@ -7,19 +7,33 @@ import threading
 def compute_height(n, parents):
     # Write this function
     max_height = 0
-    # Your code here
+    for x in range(n):
+        dzilums = 0
+        id = x
+        while id != -1:
+            dzilums = dzilums + 1
+            id = parents[id]
+        max_height = max(max_height, dzilums)
+
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    text = str(input())
+    if "I" in text:
+        skaits = int(input())
+        dati = list(map(int, input().split()))
+        print(compute_height(skaits, dati))
+
+    if "F" in text:
+        name = str(input())
+        name = "test/" + str(name)
+        file = open(name,'r')
+        skaits = int(file.readline())
+        dati = list(map(int, file.readline().split()))
+        file.close()
+        print(compute_height(skaits, dati))
+
 
 
 # In Python, the default limit on recursion depth is rather low,
